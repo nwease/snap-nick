b6a9e470910dc3900d91fc4ca693f49318bf8854
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-03-20 10:40:46 +0100

    did some styling of preview component and many other things

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,6 +4,14 @@ import {useDispatch, useSelector} from 'react-redux';
 import {resetCameraImage, selectCameraImage, setCameraImage} from '../features/cameraSlice';
 import {useHistory} from 'react-router';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
 
 function Preview() {
 
@@ -21,15 +29,33 @@ function Preview() {
         dispatch(resetCameraImage());
     }
 
+    const sendPost = () => {
+
+    }
+
     return (
         <div className='preview'>
             <CloseIcon onClick={closePreview} className='preview__close' />
 
             <div className='preview__toolbarRight'>
-
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
             </div>
 
             <img src={cameraImage} alt=''/>
+
+            <div onClick={sendPost} className='preview__footer'>
+                <h2>
+                    Send Now
+                </h2>
+
+                <SendIcon className='preview__sendIcon' />
+            </div>
         </div>
     );
 }
